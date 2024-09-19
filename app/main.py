71aacf9101fbeb6b4b7a0fcd2cec5d1b0f850306
sajosam <sajo@onepane.ai>
2024-09-19 20:53:17 +0530

    updated the main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return JSONResponse(content={"message": "Hello World"})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
